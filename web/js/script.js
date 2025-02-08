792c1760ecbc4289512ebd410ecb70de567fd9e6
EVO-14 <duduvitorinooliveira@gmail.com>
2025-02-07 21:14:28 -0300

    Tela de login e conexão com o banco

---
+++ web/js/script.js
@@ -0,0 +1,106 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menu = document.getElementById('menu');
+    const sidebar = document.getElementById('sidebar');
+    const main = document.getElementById('main');
+    const menuItems = document.querySelectorAll('.menu-item');
+    const subMenuItems = document.querySelectorAll('.submenu-item');
+
+    menu.addEventListener('click', () => {
+        sidebar.classList.toggle('menu-toggle');
+        menu.classList.toggle('menu-toggle');
+        main.classList.toggle('menu-toggle');
+    });
+
+    menuItems.forEach(item => {
+        item.addEventListener('click', (event) => {
+            event.preventDefault();
+
+            if (!sidebar.classList.contains('menu-toggle')) {
+                sidebar.classList.add('menu-toggle');
+                menu.classList.add('menu-toggle');
+                main.classList.add('menu-toggle');
+                return;
+            }
+
+            document.querySelectorAll('.menu-item.active').forEach(activeItem => {
+                if (activeItem !== item) {
+                    activeItem.classList.remove('active');
+                    let submenu = activeItem.nextElementSibling;
+                    if (submenu && submenu.classList.contains('submenu')) {
+                        submenu.style.maxHeight = '0px';
+                        submenu.classList.remove('open');
+                    }
+                }
+            });
+
+            item.classList.toggle('active');
+            let submenu = item.nextElementSibling;
+            if (submenu && submenu.classList.contains('submenu')) {
+                submenu.style.maxHeight = submenu.classList.contains('open') ? '0px' : (submenu.scrollHeight * 2) + 'px';
+                submenu.classList.toggle('open');
+            }
+
+            menuItems.forEach(el => el.classList.remove('selected'));
+            item.classList.add('selected');
+        });
+    });
+
+    subMenuItems.forEach(item => {
+        item.addEventListener('click', (event) => {
+            event.preventDefault();
+
+            if (!sidebar.classList.contains('menu-toggle')) {
+                sidebar.classList.add('menu-toggle');
+                menu.classList.add('menu-toggle');
+                main.classList.add('menu-toggle');
+                return;
+            }
+
+            document.querySelectorAll('.submenu-item.active').forEach(activeItem => {
+                if (activeItem !== item) {
+                    activeItem.classList.remove('active');
+                    let submenu = activeItem.nextElementSibling;
+                    if (submenu && submenu.classList.contains('sub-submenu')) {
+                        submenu.style.maxHeight = '0px';
+                        submenu.classList.remove('open');
+                    }
+                }
+            });
+
+            item.classList.toggle('active');
+            let submenu = item.nextElementSibling;
+            if (submenu && submenu.classList.contains('sub-submenu')) {
+                submenu.style.maxHeight = submenu.classList.contains('open') ? '0px' : (submenu.scrollHeight * 1) + 'px';
+                submenu.classList.toggle('open');
+            }
+        });
+    });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleSwitch = document.getElementById('darkMode');
+    const body = document.body;
+    const dropdownMenu = document.querySelector('.dropdown-menu');
+
+    if (localStorage.getItem('dark-mode') === 'enabled') {
+        body.classList.add('dark-mode');
+        dropdownMenu.classList.add('dropdown-menu-dark');
+        toggleSwitch.checked = true;
+    }
+
+    toggleSwitch.addEventListener('change', () => {
+        body.classList.add('transition-mode');
+
+        setTimeout(() => {
+            if (toggleSwitch.checked) {
+                body.classList.add('dark-mode');
+                dropdownMenu.classList.add('dropdown-menu-dark');
+                localStorage.setItem('dark-mode', 'enabled');
+            } else {
+                body.classList.remove('dark-mode');
+                dropdownMenu.classList.remove('dropdown-menu-dark');
+                localStorage.setItem('dark-mode', 'disabled');
+            }
+        }, 150);
+    });
+});
